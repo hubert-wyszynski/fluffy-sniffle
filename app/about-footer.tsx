3dec5680062daf4e835d9e6faee2671a9b1ba61b
hubertw <h.wyszynski@livechat.com>
2024-05-01 23:44:20 +0200

    footer adjusted

--- app/about-footer.tsx
+++ app/about-footer.tsx
@@ -5,10 +5,14 @@ import Link from 'next/link';
 export const AboutFooter = () => {
     return (
         <div className="custom-dot-container dark:custom-dot-container flex w-full flex-col sm:flex-row">
-            <div className="w-full sm:w-1/2 p-9 pb-6 sm:p-14">
-                <ul>
-                    <li className="mb-3 hover:underline"><a href="mailto:hub.wyszynski@gmail.com">hub.wyszynski@gmail.com</a></li>
-                    <li className="mb-3 hover:underline"><Link rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/hubert-wyszy%C5%84ski-685473130/">linkedin</Link></li>
+            <div className="w-full sm:w-1/2 p-9 pb-6 sm:p-14 sm:pr-0">
+                <ul className="text-sm">
+                    <li className="mb-5 mt-2 hover:underline">
+                        <a className="bg-blue-500/20 p-2 rounded-lg" href="mailto:hub.wyszynski@gmail.com">hub.wyszynski@gmail.com</a>
+                    </li>
+                    <li className="hover:underline">
+                        <Link className="bg-blue-500/20 p-2 rounded-lg" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/hubert-wyszy%C5%84ski-685473130/">also here (if you use that)</Link>
+                    </li>
                 </ul>
             </div>
             <div className="px-9 pb-9 pt-0 sm:p-14">
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         {allPosts.map((post) => (
             <article key={post._id} className="">
             <Link className="no-underline" href={post.slug}>
-                <h2 className="mb-1 text-pink-500 dark:text-pink-300 ">{post.title}</h2>
+                <h2 className="mb-1 text-blue-300 dark:text-blue-300 ">{post.title}</h2>
             </Link>
             {post.description && <p className="mt-0">{post.description}</p>}
             </article>
