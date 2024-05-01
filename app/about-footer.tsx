cd9c28ad20822c9978ac80aaaf88d8978fe28b3b
hubertw <h.wyszynski@livechat.com>
2024-05-01 02:49:58 +0200

    about footer adjustments

--- app/about-footer.tsx
+++ app/about-footer.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const AboutFooter = () => {
     return (
-        <div className="custom-dot-container dark:custom-dot-container flex w-full">
-            <div className="w-1/2 p-9 md:p-14">
+        <div className="custom-dot-container dark:custom-dot-container flex w-full flex-col sm:flex-row">
+            <div className="w-full sm:w-1/2 p-9 pb-6 sm:p-14">
                 <ul>
-                    <li className="underline mb-3"><a href="mailto:hub.wyszynski@gmail.com">hub.wyszynski@gmail.com</a></li>
-                    <li className="underline mb-3">resume</li>
+                    <li className="mb-3 hover:underline"><a href="mailto:hub.wyszynski@gmail.com">hub.wyszynski@gmail.com</a></li>
+                    <li className="mb-3 hover:underline"><Link rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/hubert-wyszy%C5%84ski-685473130/">linkedin</Link></li>
                 </ul>
             </div>
-            <div className="p-9 md:p-14">
-                <div className="flex justify-between">
-                    <div className="">
-                        <Image className="rounded-full m-0" width="190" height="190" src="/me.jpeg" alt="me" />
-                    </div>
+            <div className="px-9 pb-9 pt-0 sm:p-14">
+                <div className="flex justify-start">
+                    <Image className="rounded-full m-0 w-32 h-32 md:w-auto md:h-auto" width="190" height="190" src="/me.jpeg" alt="me" />
                 </div>
             </div>
         </div>
